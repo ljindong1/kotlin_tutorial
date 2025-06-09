164a805f8e1adcd66dd2d78e5ccde949661a6c36
ljindong <ljindong1@gmail.com>
2025-06-09 19:53:30 +0900

    표준 lib 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
